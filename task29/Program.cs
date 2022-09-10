a742ec0a00673761e19c25af460b888d166435ce
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-10 14:56:12 +0300

    Solving the problem task 29

--- task29/Program.cs
+++ task29/Program.cs
@@ -2,3 +2,22 @@
 // массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+
+int[] array = new int[8];
+int N = array.Length;
+
+for (int i = 0; i < N; i++)
+{
+    array[i] = new Random().Next(0, 10);
+
+    if (array[i] == 2) array[i] = 12345; // Простое условие 
+
+    if (array[i] == 5)         // Условие если 5 то победа
+    {
+      array[i] = 1000000;
+      Console.WriteLine("Урааа!!! Ты победил");
+    }
+
+    Console.WriteLine(array[i]);
+}

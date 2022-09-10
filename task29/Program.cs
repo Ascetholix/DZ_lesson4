11ea84a01dd23834ff1040f6f93ec8688944e1e5
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-10 15:17:03 +0300

    Исправил

--- task29/Program.cs
+++ task29/Program.cs
@@ -4,20 +4,28 @@
 // 6, 1, 33 -> [6, 1, 33]
 Console.Clear();
 
-int[] array = new int[8];
-int N = array.Length;
+Console.Write("Введите длину массива число 8: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i < N; i++)
+int[] array = new int[N];
+
+if (N == 8) // Если условия True то викторина
 {
-    array[i] = new Random().Next(0, 10);
-
-    if (array[i] == 2) array[i] = 12345; // Простое условие 
-
-    if (array[i] == 5)         // Условие если 5 то победа
+    for (int i = 0; i < N; i++)
     {
-      array[i] = 1000000;
-      Console.WriteLine("Урааа!!! Ты победил");
-    }
+        array[i] = new Random().Next(0, 10);
 
-    Console.WriteLine(array[i]);
+        if (array[i] == 2) array[i] = 12345; // Простое условие 
+
+        if (array[i] == 5)         // Условие если 5 то победа
+        {
+            array[i] = 1000000;
+            Console.WriteLine("Урааа!!! Ты победил");
+        }
+        Console.WriteLine(array[i]);
+    }
+}
+else
+{
+    Console.WriteLine("Очень жаль вы ввели не правельное число. ");
 }

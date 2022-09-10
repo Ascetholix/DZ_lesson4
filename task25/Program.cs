82e0a417b51ca3dd51e4615a266ad55d4d7b5037
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-10 14:36:41 +0300

    Solving the problem

--- task25/Program.cs
+++ task25/Program.cs
@@ -2,3 +2,15 @@
 //  и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+Console.Write("Введите число А : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B : ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int res = 1;
+
+for (int i = 0; i < b; i++)
+{
+    res *= a;
+}
+Console.WriteLine($"{a} в степени {b} = {res}");
--- task27/Program.cs
+++ task27/Program.cs
@@ -3,3 +3,17 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numPart = 0;
+int sum = 0;
+
+for (int i = 0; num != 0; i++)
+{
+    numPart = num % 10;
+    num /= 10;
+    sum += numPart;
+}
+Console.WriteLine($"{num} сумма цифр равно: {sum}");
